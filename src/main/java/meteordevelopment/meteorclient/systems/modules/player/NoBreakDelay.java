c9536f6ed5adff719d67b069a052d5ee238a392d
mankool <8884398+mankool0@users.noreply.github.com>
2022-11-20 02:57:07 -0800

    Undo my useless NoBreakDelay stuff

--- src/main/java/meteordevelopment/meteorclient/systems/modules/player/NoBreakDelay.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/player/NoBreakDelay.java
@@ -5,19 +5,11 @@
 
 package meteordevelopment.meteorclient.systems.modules.player;
 
-import meteordevelopment.meteorclient.events.entity.player.PlayerMoveEvent;
-import meteordevelopment.meteorclient.mixin.ClientPlayerInteractionManagerAccessor;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.orbit.EventHandler;
 
 public class NoBreakDelay extends Module {
     public NoBreakDelay() {
         super(Categories.Player, "no-break-delay", "Completely removes the delay between breaking blocks.");
-    }
-
-    @EventHandler
-    private void onPlayerMove(PlayerMoveEvent event) {
-        ((ClientPlayerInteractionManagerAccessor) mc.interactionManager).setBlockBreakingCooldown(0);
     }
 }
